97f8035ea207dd3ea1faf2b21f047a6ad3bc1d27
geofpwhite <87089088+geoffreypwhite@users.noreply.github.com>
2023-12-01 11:13:04 -0500

    cleanup part1&part2

--- advent1/advent1.go
+++ advent1/advent1.go
@@ -7,12 +7,36 @@ import (
 	"strings"
 )
 
-func main() {
+func part1() {
 	content, err := ioutil.ReadFile("input.txt") // the file is inside the local directory
 	if err != nil {
 		fmt.Println("Err")
 	}
-	// fmt.Println(string(content))
+
+	sum := 0
+
+	ar := strings.Split(string(content), "\n")
+	for _, c := range ar {
+
+		s := string(c)
+		fmt.Println(s)
+		x := firstDigit(s, false)
+
+		fmt.Println(x, " x")
+		s = Reverse(s)
+		y := firstDigit(s, true)
+		sum += (x * 10) + y
+
+	}
+	fmt.Println(sum)
+}
+
+func part2() {
+	content, err := ioutil.ReadFile("input.txt") // the file is inside the local directory
+	if err != nil {
+		fmt.Println("Err")
+	}
+
 	sum := 0
 
 	ar := strings.Split(string(content), "\n")
@@ -40,9 +64,6 @@ func main() {
 		fmt.Println(x, " x")
 		s = Reverse(s)
 		y := firstDigit(s, true)
-		// fmt.Print(s)
-		// fmt.Println(x)
-		// fmt.Print(y)
 		sum += (x * 10) + y
 
 	}
@@ -53,14 +74,6 @@ func main() {
 func firstDigit(s string, reverse bool) int {
 
 	for _, c := range s {
-		// fmt.Printf("%d %s\n", i, c)
-		// fmt.Printf(string(c) + " c")
-
-		// if len(stringNumberIndices) > 0 {
-		// 	if index > stringNumberIndices[0] {
-		// 		return stringNumberValues[0]
-		// 	}
-		// }
 		if strings.Contains("0123456789", string(c)) {
 			fmt.Println(string(c) + "  c")
 			i, err := strconv.Atoi(string(c))
